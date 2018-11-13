6fbfe22647e836d5c92d9215947a90cde4b89477
davidhaoming <haomingdave@hotmail.com>
2018-11-13 12:10:09 +0800

    test

--- config/test.env.js
+++ config/test.env.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
+  NODE_ENV: '"test"',
   BASE_API: '"https://test.hengdaomed.com"',
   OSS_URL: '"http://wsi.oss-cn-shanghai.aliyuncs.com"'
 })
